ae23e8034cc42c7bde3c8f3c07f02d808c4dae72
José Nery <josenerydev@gmail.com>
2020-04-04 05:58:43 -0300

    chapter 22

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import dataStore from "./store";
 import { Selector } from "./Selector";
-import { ProductDisplay } from "./ProductDisplay";
-import { SupplierDisplay } from "./SupplierDisplay";
+// import { ProductDisplay } from "./ProductDisplay";
+// import { SupplierDisplay } from "./SupplierDisplay";
+import { PRODUCTS, SUPPLIERS } from "./store/dataTypes";
 
 export default class App extends Component {
 
     render() {
-        return <Provider store={dataStore}>
-            <Selector>
-                <ProductDisplay name="Products" />
-                <SupplierDisplay name="Suppliers" />
-            </Selector>
-        </Provider>
+        return  <Provider store={ dataStore }>
+                    <Selector>
+                        <data name="Products" datatype={ PRODUCTS } />
+                        <data name="Suppliers" datatype ={ SUPPLIERS } />
+                    </Selector>      
+                </Provider>                            
     }
 }

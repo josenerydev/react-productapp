e96adbdd9275db3b1c871274eb50c2487b82c947
José Nery <josenerydev@gmail.com>
2020-04-04 11:39:36 -0300

    chapter 23 - Consuming a Web Service

--- src/Selector.js
+++ src/Selector.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } 
+import { BrowserRouter as Router, Route, Switch, Redirect }
     from "react-router-dom";
 import { ToggleLink } from "./routing/ToggleLink";
 //import { CustomPrompt } from "./routing/CustomPrompt";
 import { RoutedDisplay } from "./routing/RoutedDisplay";
+import { IsolatedTable } from "./IsolatedTable";
+import { IsolatedEditor } from './IsolatedEditor';
+import { RequestError } from "./webservice/RequestError";
 
 export class Selector extends Component {
 
@@ -11,31 +14,38 @@ export class Selector extends Component {
 
         const routes = React.Children.map(this.props.children, child => ({
             component: child,
-            name: child.props.name,            
+            name: child.props.name,
             url: `/${child.props.name.toLowerCase()}`,
             datatype: child.props.datatype
         }));
 
-        return <Router getUserConfirmation={ this.customGetUserConfirmation }>
+        return <Router getUserConfirmation={this.customGetUserConfirmation}>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-2">            
-                        { routes.map(r => <ToggleLink key={ r.url } to={ r.url }>
-                                            { r.name }
-                                        </ToggleLink>)}
+                    <div className="col-2">
+                        <ToggleLink to="/isolated">Isolated Data</ToggleLink>
+                        {routes.map(r => <ToggleLink key={r.url} to={r.url}>
+                            {r.name}
+                        </ToggleLink>)}
                     </div>
                     <div className="col">
                         <Switch>
-                            { routes.map(r => 
-                               <Route key={ r.url } 
-                                   path={ `/:datatype(${r.datatype})/:mode?/:id?`}
-                                   component={ RoutedDisplay(r.datatype)} />
+                            <Route path="/isolated" component={IsolatedTable}
+                                exact={true} />
+                            <Route path="/isolated/:mode/:id?"
+                                component={IsolatedEditor} />
+                            <Route path="/error/:message"
+                                component={RequestError} />
+                            {routes.map(r =>
+                                <Route key={r.url}
+                                    path={`/:datatype(${r.datatype})/:mode?/:id?`}
+                                    component={RoutedDisplay(r.datatype)} />
                             )}
-                            <Redirect to={ routes[0].url } />
+                            <Redirect to={routes[0].url} />
                         </Switch>
                     </div>
                 </div>
             </div>
-        </Router>        
+        </Router>
     }
 }
